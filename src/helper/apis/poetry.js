322d9157aa74219f3f90adac6599c11047828457
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2018-11-26 13:40:04 +0800

    :bug: fix some interface

--- src/helper/apis/poetry.js
+++ src/helper/apis/poetry.js
@@ -4,21 +4,13 @@ import $ajax from '../ajax'
 import $utils from '../utils'
 
 export default {
-  search(params) {
+  searchPoetry(params) {
     return $ajax.get($utils.composeApiPath('searchPoetry'), params)
   },
-  getDetail(params) {
-    const p = {
-      _id: '5beaeea4256c4e2494227a50',
-      title: '临洛水',
-      author: '太宗皇帝',
-      paragraphs: [
-        '春搜驰骏骨，总辔俯长河。',
-        '霞处流萦锦，风前瀁卷罗。',
-        '水花翻照树，堤兰倒插波。',
-        '岂必汾阴曲，秋云发棹歌。'
-      ]
-    }
-    return Promise.resolve(p)
+  getMorePoetry(data) {
+    return $ajax.get($utils.composeApiPath('getMorePoetry'), data)
+  },
+  getPoetryById(data) {
+    return $ajax.get($utils.composeApiPath('getPoetryById'), data)
   }
 }
